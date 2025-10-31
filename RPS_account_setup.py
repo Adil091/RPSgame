9cb7af6377730e91e53a85c6c123ce91a304827a
adil <adilmulla091@gmail.com>
2025-10-31 18:58:53 +0530

    created a account program to resgister the user name and password

--- RPS_account_setup.py
+++ RPS_account_setup.py
@@ -1,10 +1,12 @@
 print("------------------------------------------")
-print("rock", "paper", "scissor","Account","setup")
+print("🤘rock,", "✋paper,", "✂scissor,", "🧾Account,", "🛠setup")
 print("------------------------------------------")
 while True:
   username = input("Pick your username: ")
   password = input("Pick your password: ")
   password_confirm = input("Confirm your password: ")
+  if password != password_confirm:
+      print("Passwords do not match. Please try again.")
   if password == password_confirm:
       print("Account created successfully!")
       text_file = open("accounts.csv", "a")
@@ -16,5 +18,3 @@ while True:
       text_file.write(username + "," + password + "\n")
       text_file.close()
       break
-  else:
-      print("Passwords do not match. Please try again.")
